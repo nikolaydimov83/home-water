5e014eabaaff20e8ade3a13e39a581e5b538c101
Nikolay Dimov <nikolay.dimov83@gmail.com>
2026-06-02 23:50:08 +0300

    Fix

--- find_relays.py
+++ find_relays.py
@@ -3,6 +3,8 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+
+#! 26 is the rightmost relay when you are seeing the relays from the green part
 pins = [26, 20, 21]
 
 for p in pins:
@@ -12,9 +14,9 @@ try:
     for p in pins:
         print(f'Testing GPIO {p}...')
         GPIO.output(p, GPIO.LOW)
-        time.sleep(0.3)
+        time.sleep(5)
         GPIO.output(p, GPIO.HIGH)
-        time.sleep(0.2)
+        time.sleep(0)
 except KeyboardInterrupt:
     print("Stopped")
 finally:
